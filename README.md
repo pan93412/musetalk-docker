dd962710e17f67e61dd9dfe12ec28787235db35a
pan93412 <pan93412@gmail.com>
2025-05-07 21:00:11 +0800

    fix: add ghcr.io to image prefix

--- README.md
+++ README.md
@@ -17,9 +17,5 @@ It is handled by CI.
 ### Running
 
 ```bash
-# ghcr.io
 docker run --gpus all -p 7860:7860 ghcr.io/pan93412/musetalk-docker:1.5-gradio
-
-# local by build.sh
-docker run --gpus all -p 7860:7860 pan93412/musetalk-docker:1.5-gradio
 ```
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -eu
 
 echo "Building base image..."
-docker build -t pan93412/musetalk-docker:1.5-base -f Dockerfile.base .
+docker build -t ghcr.io/pan93412/musetalk-docker:1.5-base -f Dockerfile.base .
 
 echo "Building application image..."
-docker build -t pan93412/musetalk-docker:1.5-gradio -f Dockerfile .
+docker build -t ghcr.io/pan93412/musetalk-docker:1.5-gradio -f Dockerfile .
